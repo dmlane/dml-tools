e09e585dc4878a6b6b3cc6027038730eb0e67ee5
David Lane <git2018@dmlane.net>
2025-07-14 17:10:32 +0100

    Latest

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Nix flake for dml-tools (Poetry CLI using Python 3.12)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems
+        (system: f { pkgs = import nixpkgs { inherit system; }; system = system; });
+    in {
+      packages = forAllSystems ({ pkgs, system }: {
+        default = pkgs.python312Packages.buildPythonPackage {
+          pname = "dml-tools";
+          version = "2024.1.1025";
+          format = "pyproject";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.poetry ];
+          propagatedBuildInputs = with pkgs.python312Packages; [
+            eyed3
+            appdirs
+          ];
+
+          pythonImportsCheck = [ "tools.car_podcasts" ];
+
+          meta = {
+            description = "Set of command-line tools (dml-tools)";
+            homepage = "https://github.com/dmlane/dml-tools";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+      });
+
+      devShells = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.poetry
+            pkgs.python312
+          ];
+        };
+      });
+    };
