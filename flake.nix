087810e741f7e0464b7e84b6bfbc9fc502cca864
David Lane <git2018@dmlane.net>
2025-07-15 07:42:23 +0100

    bump version 2025.7.1032 -> 2025.7.1033

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         let
           dmlToolsPkg = pkgs.python312Packages.buildPythonPackage {
             pname = "dml-tools";
-            version = "2025.7.1032";
+            version = "2025.7.1033";
             format = "pyproject";
 
             src = ./.;
